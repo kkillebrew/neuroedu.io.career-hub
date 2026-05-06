d17959a3378ce6586bb3ae693a39cfc7b0dad3ca
kkillebr <kkillebr@umn.edu>
2026-05-05 19:27:47 -0700

    cleaning up the sidebar mess

--- career_hub_sidebar.py
+++ career_hub_sidebar.py
@@ -177,19 +177,19 @@ def render_sidebar():
 
         presence_html = f"""
         <div class="presence-bar">
-            <a href="https://linkedin.com/in/yourprofile" target="_blank" class="presence-icon" title="LinkedIn">
+            <a href="linkedin.com/in/kylewkillebrew" target="_blank" class="presence-icon" title="LinkedIn">
                 <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
             </a>
-            <a href="https://github.com/yourprofile" target="_blank" class="presence-icon" title="GitHub">
+            <a href="https://github.com/kkillebrew/" target="_blank" class="presence-icon" title="GitHub">
                 <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.332-5.467-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
             </a>
-            <a href="https://scholar.google.com/citations?user=yourid" target="_blank" class="presence-icon" title="Google Scholar">
+            <a href="https://scholar.google.com/citations?user=y-2G-voAAAAJ&hl=en" target="_blank" class="presence-icon" title="Google Scholar">
                 <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M12 24a7 7 0 1 1 0-14 7 7 0 0 1 0 14zm0-24L0 9.5l4.838 3.911L12 17.428l7.162-4.017v6.611h2v-8.156L24 9.5z"/></svg>
             </a>
-            <a href="https://orcid.org/your-orcid" target="_blank" class="presence-icon" title="ORCID">
+            <a href="https://orcid.org/0000-0002-9662-9844" target="_blank" class="presence-icon" title="ORCID">
                 <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M12 0C5.372 0 0 5.372 0 12s5.372 12 12 12 12-5.372 12-12S18.628 0 12 0zM7.369 4.378c.525 0 .947.431.947.947s-.422.949-.947.949a.95.95 0 0 1-.949-.949c0-.516.424-.947.949-.947zm-.722 3.038h1.444v10.041H6.647V7.416zm3.562 0h3.9c3.712 0 5.344 2.653 5.344 5.025 0 2.578-2.016 5.025-5.325 5.025h-3.919V7.416zm1.444 1.303v7.444h2.297c3.272 0 4.022-2.484 4.022-3.722 0-2.016-1.284-3.722-4.097-3.722h-2.222z"/></svg>
             </a>
-            <a href="https://twitter.com/yourhandle" target="_blank" class="presence-icon" title="X (Twitter)">
+            <a href="https://twitter.com/jollygreen16" target="_blank" class="presence-icon" title="X (Twitter)">
                 <svg width="22" height="22" viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>
             </a>
             <a href="https://www.wyzant.com/Tutors/yourprofile" target="_blank" class="presence-icon" title="Wyzant Tutoring">
